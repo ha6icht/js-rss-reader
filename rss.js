53068d798ce621d72cfdfd2ed4466205373d1faf
ha6icht <rolf.karlen@gmail.com>
2019-10-05 20:04:32 +0200

    only xml parsing

--- rss.js
+++ rss.js
@@ -1,5 +1,4 @@
 const DOMPARSER = new DOMParser();
-let doc = '';
 /* Fetch URLs from JSON */
 fetch('urls.json').then((res) => {
 	res.text().then((data) => {
@@ -14,19 +13,32 @@ fetch('urls.json').then((res) => {
 				return;
 			}
 			fetch(url, {mode: 'no-cors'}).then((res) => {
-				res.text().then((xmlText) => {
-					/* Extract the RSS Feed URL from the website */
+				res.text().then((xmlTxt) => {
+					/* Fetch the RSS Feed */
 					try {
-						doc = DOMPARSER.parseFromString(xmlText, 'text/xml');
-						//var feedUrl = doc.querySelector('link[type="application/rss+xml"]').href;
-					} catch (e) {
-						console.error('Error in parsing the website');
+						/* Parse the RSS Feed and display the content */
+						let doc = DOMPARSER.parseFromString(xmlTxt, 'text/xml');
+						let heading = document.createElement('h1');
+						heading.textContent = url.hostname;
+						frag.appendChild(heading);
+						doc.querySelectorAll('item').forEach((item) => {
+							let temp = document.importNode(document.querySelector('template').content, true);
+							let i = item.querySelector.bind(item);
+							let t = temp.querySelector.bind(temp);
+							t('h2').textContent = !!i('title') ? i('title').textContent : '-';
+							t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
+							t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
+							t('h3').textContent = url.hostname;
+							frag.appendChild(temp);
+						})
+					}catch (e) {
+						console.error('Error in parsing the feed');
 						return;
 					}
-					/* Fetch the RSS Feed */
-					fetch(doc).then((res) => {
+					
+					/*fetch(doc).then((res) => {
 						res.text().then((xmlTxt) => {
-							/* Parse the RSS Feed and display the content */
+							
 							try {
 								let doc = DOMPARSER.parseFromString(xmlTxt, "text/xml");
 								let heading = document.createElement('h1');
@@ -52,8 +64,8 @@ fetch('urls.json').then((res) => {
 							document.querySelector('output').appendChild(frag);
 						})
 					}).catch(() => console.error('Error in fetching the RSS feed'))
-				})
-			}).catch(() => console.error('Error in fetching the website'));
+				})*/
+			}).catch(() => console.error('Error in fetching the xml-site'));
 		})
 	})
 }).catch(() => console.error('Error in fetching the URLs json'));
