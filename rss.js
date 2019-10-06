3e5a273d57396022c02dea493cececf77bcdbf51
ha6icht <rolf.karlen@gmail.com>
2019-10-06 10:27:20 +0200

    3 article

--- rss.js
+++ rss.js
@@ -29,19 +29,25 @@ fetch('urls.json').then((res) => {
 						res.text().then((xmlTxt) => {
 							/* Parse the RSS Feed and display the content */
 							try {
+								let c = 0;
+								let diff = 0;
 								let doc = DOMPARSER(xmlTxt, "text/xml");
 								let heading = document.createElement('h1');
 								heading.textContent = url.hostname;
 								frag.appendChild(heading);
 								doc.querySelectorAll('item').forEach((item) => {
-									let temp = document.importNode(document.querySelector('template').content, true);
-									let i = item.querySelector.bind(item);
-									let t = temp.querySelector.bind(temp);
-									t('h2').textContent = !!i('title') ? i('title').textContent : '-';
-									t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
-									t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
-									t('h3').textContent = url.hostname;
-									frag.appendChild(temp);
+									c++;
+									diff = item.length - 3;
+									if(diff>c){
+										let temp = document.importNode(document.querySelector('template').content, true);
+										let i = item.querySelector.bind(item);
+										let t = temp.querySelector.bind(temp);
+										t('h2').textContent = !!i('title') ? i('title').textContent : '-';
+										t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#';
+										t('p').innerHTML = !!i('description') ? i('description').textContent : '-';
+										t('h3').textContent = url.hostname;
+										frag.appendChild(temp);
+									}
 								})
 							} catch (e) {
 								console.error('Error in parsing the feed');
