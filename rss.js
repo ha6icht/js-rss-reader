d983bb99399ae63540ec1df8c86439142021c8ca
ha6icht <rolf.karlen@gmail.com>
2019-10-06 09:23:05 +0200

    changed textContent output

--- rss.js
+++ rss.js
@@ -46,6 +46,7 @@ fetch('urls.json').then((res) => {
 								console.error('Error in parsing the feed');
 							}
 							if(hasBegun) {
+								document.querySelector('output').textContent = '';
 								document.querySelector('.loader').style.display = 'none'; 
 								hasBegun = false;
 							}
