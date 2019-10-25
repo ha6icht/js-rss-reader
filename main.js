9b19c0ace2409340c8ce2cd9a31410b87e6d359d
ha6icht <rolf.karlen@gmail.com>
2019-10-25 09:11:22 +0200

    IE ok

--- main.js
+++ main.js
@@ -6,21 +6,18 @@ document.querySelector('output').style.display = 'none';
 let frag = document.createDocumentFragment();
 
 function getRequest(url) {
-	if (window.XMLHttpRequest) {
-		var xhr = new XMLHttpRequest();
-	}
-	else if (window.ActiveXObject) {
-		var xhr = new ActiveXObject("Microsoft.XMLHTTP");
-	}
+	//console.log('xhr');
+	var xhr = new XMLHttpRequest();
 	
 	//xhr.responseType = 'text';
 
 	xhr.onreadystatechange = function (){
 		if (xhr.readyState == 4){
-			renderResponse(xhr.response);
+			renderResponse(xhr.responseText);
 		}
 	}
 	xhr.open('GET', url);
+	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 	xhr.send();	
 }
 
